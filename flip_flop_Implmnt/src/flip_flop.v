4cd70a537dac195add969e2ca3c6bfedefb99d62
Allan Regush <17693494+AllanRegush@users.noreply.github.com>
2022-05-18 20:31:58 -0600

    Debounce Switch

--- flip_flop_Implmnt/src/flip_flop.v
+++ flip_flop_Implmnt/src/flip_flop.v
@@ -3,16 +3,22 @@ module Flip_Flop
 	 input i_Switch_2,
 	 output o_LED_2);
 
-	reg r_LED_2    = 1'b0;
-	reg r_Switch_2 = 1'b0;
+	reg  r_LED_2    = 1'b0;
+	reg  r_Switch_2 = 1'b0;
+	wire w_Switch_2;
+
+	Debounce_Switch Instance
+		(.i_Clk(i_Clk),
+		 .i_Switch(i_Switch_2),
+		 .o_Switch(w_Switch_2));
 
 	always @(posedge i_Clk)
 	begin
-		r_Switch_2 <= i_Switch_2;
+		r_Switch_2 <= w_Switch_2;
 		
-		if (i_Switch_2 == 1'b0 && r_Switch_2 == 1'b1)
+		if (w_Switch_2 == 1'b0 && r_Switch_2 == 1'b1)
 		begin
-			r_LED_2 = ~r_LED_1;
+			r_LED_2 = ~r_LED_2;
 		end
 	end
 
